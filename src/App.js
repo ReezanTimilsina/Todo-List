e000b01bfc9d99a1fc47b512e25a647e2a50be99
Rijan Timilsina <rijantimilsina@Rijans-MacBook-Air.local>
2023-07-06 20:17:46 +0545

    Todo app Completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components/pages/home/HomePage';
-import AboutPage from './components/pages/about/AboutPage';
-import PasswordsPage from './components/pages/passwords/PasswordsPage';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+import Form from './components/Form'
+import TodosList from './components/TodosList'
 
-export default function App() {
+const App = () => {
+
+  const initialState = JSON.parse(localStorage.getItem("todos")) || []; 
+  const [input, setInput ] = useState('');
+  const [todos, setTodos] = useState(initialState);
+  const [editTodo, setEditTodo] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/passwords" element={<PasswordsPage />} />
-        </Routes>
+  <div className='container'>
+    <div className='app-wrapper'>
+      <div>
+        <Header />
       </div>
-    </Router>
-  );
+      <div>
+        <Form
+        input={input}
+        setInput={setInput}
+        todos={todos}
+        setTodos={setTodos}
+        editTodo={editTodo}
+        setEditTodo={setEditTodo} /> 
+      </div>
+      <div>
+        <TodosList 
+        todos={todos} 
+        setTodos={setTodos} 
+        setEditTodo={setEditTodo}
+        />
+      </div>
+    </div>
+  </div>
+  )
 }
+
+export default App
